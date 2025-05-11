c4bc5caa64d3739a02ce16184db6c993e49dc5a1
Larry Besant <larrybesant@gmail.com>
2025-05-11 11:59:11 -0500

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+// Sidebar.js
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>Contacts</h2>
+      <ul>
+        <li>Friend 1</li>
+        <li>Friend 2</li>
+        <li>Friend 3</li>
+        {/* Add more contacts as needed */}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
