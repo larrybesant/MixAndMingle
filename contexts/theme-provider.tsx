7c5552108c29e9ca6db8762bd06eb01c32c267c7
Larry Besant <larrybesant@gmail.com>
2025-06-17 09:01:43 -0500

    Fix ThemeProvider to accept next-themes props

--- contexts/theme-provider.tsx
+++ contexts/theme-provider.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
-import { ReactNode } from 'react';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps & { children: ReactNode }) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
 
